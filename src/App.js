9b096dcc71f57284b3f85e9a219dbab9529caab3
Khánh Trần Quốc <you@example.com>
2022-08-01 09:17:26 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Trần Quốc Khánh 1911066162 19DTHD3
+        </p>
+        <p>
+          Dưới đây là fb mình nhé !
+        </p>
+        <a
+          className="App-link"
+          href="https://www.facebook.com/luns.nkocs.9"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Nhấp đi đừng ngại !
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
